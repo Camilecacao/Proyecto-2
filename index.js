f89fb29d828f16ddf7625afa2ab25487301f8f75
Camila Espinoza Jaques <caespinoza.dwfs9@bootcampudd.cl>
2023-08-12 19:44:33 -0400

    incorporación de f(x) eliminar

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ const tbody= document.getElementById ("tbody")
 // console.log (nombre)
 
 let registro= []
+let idEditando= null
 
 function guardarDatos (event){
     event.preventDefault ()
     const datos={
+        id: Date.now(),
         nombre: nombre.value,
         correo: email.value,
         telefono: telefono.value,
     }
+    console.log(datos)
+
     registro.push(datos)
     guardarLs ()
     console.log (registro)
@@ -27,6 +31,7 @@ function guardarDatos (event){
 
 
 const dibujar= ()=> {
+    obtenerLs()
     tbody.innerHTML= ""
     registro.forEach ((humano)=>{
     tbody.innerHTML+= `
@@ -35,8 +40,8 @@ const dibujar= ()=> {
     <td>${humano.correo}</td>
     <td>${humano.telefono}</td>
     <td>
-    <button class= "btn btn-info">editar</button>
-    <button class= "btn btn-danger">eliminar</button>
+    <button onclick="editar('${humano.id}')"class= "btn btn-info">editar</button>
+    <button onclick="eliminar('${humano.id}')" class= "btn btn-danger">eliminar</button>
     </td>
     </tr>
     `
@@ -47,8 +52,28 @@ const guardarLs= ()=> {
     localStorage.setItem("usuarios",JSON.stringify(registro))
 }
 
+const obtenerLs= ()=> {
+    registro=JSON.parse(localStorage.getItem("usuarios")) || []
+        
+}
 
+dibujar()
 
+const eliminar=(id)=>{
+    const indice=registro.findIndex((humano)=>humano.id==id)
+    registro.splice(indice,1)
+    guardarLs()
+    dibujar()
+}
 
+const editar=(id)=>{
+    idEditando=id
+    const indice=registro.findIndex ((humano)=>humano.id==id)
+    const humanoEditando=registro[indice]
+    nombre.value=humanoEditando.nombre
+    email.value=humanoEditando.correo
+    telefono.value=humanoEditando.telefono
+
+}
 
 form.addEventListener("submit", guardarDatos)
